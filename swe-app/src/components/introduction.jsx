ce9f357a0b1951543973a87548e62e24e2267c8c
asumpon <asumpon@smu.edu>
2020-07-16 21:46:29 -0500

    highlighted introduction

--- swe-app/src/components/introduction.jsx
+++ swe-app/src/components/introduction.jsx
@@ -14,7 +14,9 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Welcome to <br />SMU SWE <br />2020-2021</h1>
+                          
+                          <h1><mark>Welcome to SMU SWE</mark></h1>
+                          
                           <p><a className="btn btn-primary btn-learn" href="http://smu.swe.org" target="_blank" rel="noopener noreferrer">Visit SMU.SWE.ORG <i className="icon-monitor" /></a></p>
                         </div>
                       </div>
@@ -29,7 +31,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>The Hart<br /> of Lyle</h1>
+                          <h1><mark>The <b></b>Hart of Lyle</mark></h1>
                           <p><a className="btn btn-primary btn-learn" href="https://www.smu.edu/lyle" target="_blank" rel="noopener noreferrer">Visit Lyle <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
@@ -44,7 +46,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>Submit <br/>Your Resume </h1>
+                          <h1><mark>Submit <br/>Your Resume</mark></h1>
                           <p><a className="btn btn-primary btn-learn" href="https://forms.office.com/Pages/ResponsePage.aspx?id=Lg9FD08zrUuoXJrfdgUdi2k4SeMphwNDnP9zJUYjeTdUNVRaME9MVE5WNjhNVjBJV1hEQ0ZWWEJRUS4u" target="_blank" rel="noopener noreferrer">View Resume Book Form <i className="icon-book" /></a></p>
                         </div>
                       </div>
