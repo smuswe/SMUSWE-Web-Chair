ab8a302d1c7e3da2e4dc06bd3885a281f3f45d23
asumpon <asumpon@smu.edu>
2020-07-20 21:31:31 -0500

    akslf

---
+++ build/precache-manifest.0b1e4e7a38167c7bed02b6d5ac73de7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06750522ae3e08d1002f042346421e7b",
+    "url": "/2020-21/index.html"
+  },
+  {
+    "revision": "d199427a4210e505dc3c",
+    "url": "/2020-21/static/css/main.f5de08f0.chunk.css"
+  },
+  {
+    "revision": "8c8517f65a2a67a93138",
+    "url": "/2020-21/static/js/2.ee0bbc28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-21/static/js/2.ee0bbc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d199427a4210e505dc3c",
+    "url": "/2020-21/static/js/main.0103d88e.chunk.js"
+  },
+  {
+    "revision": "96b7812446fade3dbc58",
+    "url": "/2020-21/static/js/runtime-main.c89dc1c7.js"
+  }
+]);
