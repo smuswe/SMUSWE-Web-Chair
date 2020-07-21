c9bc0c4777d2721b42934ae7513d2ceafcff4e85
asumpon <asumpon@smu.edu>
2020-07-20 20:38:18 -0500

    Updates

---
+++ precache-manifest.26729d9af8e4dff775ff5e50597c76df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fb9d6cacb2489d49a46f26b1f17312",
+    "url": "/SMU-SWE-Web-Chair/index.html"
+  },
+  {
+    "revision": "c1d44fdb360c1085a0aa",
+    "url": "/SMU-SWE-Web-Chair/static/css/main.f5de08f0.chunk.css"
+  },
+  {
+    "revision": "8c8517f65a2a67a93138",
+    "url": "/SMU-SWE-Web-Chair/static/js/2.ee0bbc28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SMU-SWE-Web-Chair/static/js/2.ee0bbc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d44fdb360c1085a0aa",
+    "url": "/SMU-SWE-Web-Chair/static/js/main.27831ecd.chunk.js"
+  },
+  {
+    "revision": "05104907b43bfa4b21e2",
+    "url": "/SMU-SWE-Web-Chair/static/js/runtime-main.1ff29343.js"
+  }
+]);
